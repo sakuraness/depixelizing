b54ad8e5bbef1a4fbe12539cea035f32238a9381
lachi <rabit66119929@gmail.com>
2018-06-06 18:04:21 +0800

    2018.8.8.18.04

--- check_curves.m
+++ check_curves.m
@@ -8,6 +8,8 @@
     3 4
 %}
 function n = check_curves(i,j,tmp,from)
+    global check;
+    check(i,j)=1;
     if(from==0)
         n=0;
         if(sum(tmp(i,j,:))<=2)
@@ -29,14 +31,22 @@ function n = check_curves(i,j,tmp,from)
             for k=1:8
                 if(k==from),continue;
                 else
-                    if    (tmp(i,j,k)==1&&k==1),n=n+check_curves(i-1,   j-1,   tmp,8);
-                    elseif(tmp(i,j,k)==1&&k==2),n=n+check_curves(i-1,   j,     tmp,7);
-                    elseif(tmp(i,j,k)==1&&k==3),n=n+check_curves(i-1,   j+1,   tmp,6);
-                    elseif(tmp(i,j,k)==1&&k==4),n=n+check_curves(i,     j-1,   tmp,5);
-                    elseif(tmp(i,j,k)==1&&k==5),n=n+check_curves(i,     j+1,   tmp,4);
-                    elseif(tmp(i,j,k)==1&&k==6),n=n+check_curves(i+1,   j-1,   tmp,3);
-                    elseif(tmp(i,j,k)==1&&k==7),n=n+check_curves(i+1,   j,     tmp,2);
-                    elseif(tmp(i,j,k)==1&&k==8),n=n+check_curves(i+1,   j+1,   tmp,1);
+                    if    (tmp(i,j,k)==1&&k==1)
+                        if(check(i-1,j-1)==0),n=n+check_curves(i-1,   j-1,   tmp,8);end
+                    elseif(tmp(i,j,k)==1&&k==2)
+                        if(check(i-1,j  )==0),n=n+check_curves(i-1,   j,     tmp,7);end
+                    elseif(tmp(i,j,k)==1&&k==3)
+                        if(check(i-1,j+1)==0),n=n+check_curves(i-1,   j+1,   tmp,6);end
+                    elseif(tmp(i,j,k)==1&&k==4)
+                        if(check(i,  j-1)==0),n=n+check_curves(i,     j-1,   tmp,5);end
+                    elseif(tmp(i,j,k)==1&&k==5)
+                        if(check(i,  j+1)==0),n=n+check_curves(i,     j+1,   tmp,4);end
+                    elseif(tmp(i,j,k)==1&&k==6)
+                        if(check(i+1,j-1)==0),n=n+check_curves(i+1,   j-1,   tmp,3);end
+                    elseif(tmp(i,j,k)==1&&k==7)
+                        if(check(i+1,j  )==0),n=n+check_curves(i+1,   j,     tmp,2);end
+                    elseif(tmp(i,j,k)==1&&k==8)
+                        if(check(i+1,j+1)==0),n=n+check_curves(i+1,   j+1,   tmp,1);end
                     end
                 end
             end
--- curves.m
+++ curves.m
@@ -1,5 +1,8 @@
 function [out1,out2] = curves(fir,sec,points,tmp)
+    global check;
+    check=uint8(zeros(size(tmp,1),size(tmp,2)));
     a=check_curves(points(1,1),points(1,2),tmp,0);
     b=check_curves(points(2,1),points(2,2),tmp,0);
     if(a>b),out1=fir+a-b;out2=sec;else,out1=fir;out2=sec+b-a;end
+    clear global check;
 end
